be156b8bb248c331aa01d6cacd50a242bc8828c5
JCGarcia25 <johan.caicedo.garcia@unillanos.edu.co>
2025-09-20 09:25:07 -0500

    impuestos

--- src/ejercicios/04-desestructurador.ts
+++ src/ejercicios/04-desestructurador.ts
@@ -45,4 +45,49 @@ console.log("Frutas: " + (frutas[3] || 'No se ha encontrado'));
 const [p1, p2, p3] = frutas;
 console.log(p1, p2, p3);
 
+interface Producto {
+    descripcion: string;
+    precio: number;
+}
+
+const celular: Producto = {
+    descripcion: "iPhone 17 Pro Max 256",
+    precio: 8000000
+}
+
+const computador: Producto = {
+    descripcion: "Mac",
+    precio: 5000000
+}
+
+const carrito: Producto[] = [celular, computador];
+const impuesto: number = 0.19;
+
+interface ImpuestosOptions {
+    impuesto: number;
+    productos: Producto[];
+}
+
+function calcularImpuesto(options: ImpuestosOptions): number[] {
+    let total: number = 0;
+    options.productos.forEach(({ precio }) => {
+        total += precio
+    })
+
+    return [total, total * options.impuesto];
+}
+
+const productos: ImpuestosOptions = {
+    impuesto: 25,
+    productos: carrito
+}
+
+const totalPagar1: number[] = calcularImpuesto( productos );
+const totalPagar2: number[] = calcularImpuesto({
+    impuesto: 45,
+    productos: carrito
+});
+
+console.log({totalPagar1, totalPagar2})
+
 export {};
